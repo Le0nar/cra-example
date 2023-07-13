7d6c1032f9ad209c2c36ebcd5f1ab87fb269351d
Le0nar <yaroslavtrefilov5@gmail.com>
2023-07-13 18:47:39 +0400

    feat: add getData function

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { getData } from './utils/get-data';
+
+console.log(getData())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
